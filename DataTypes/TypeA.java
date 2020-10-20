4613f9b78a2085251a248a65a006f59f8c0675cd
Rohanpatil932 <72926252+Rohanpatil932@users.noreply.github.com>
2020-10-20 21:06:47 +0530

    data types

---
+++ DataTypes/TypeA.java
@@ -0,0 +1,35 @@
+public class TypeA{
+	public static void main(String[] show){
+		byte monthsInAYear = 12;
+		System.out.println("monthsInAYear:"+monthsInAYear);
+		
+		short priceOfMySleeper = 499;
+		System.out.println("priceOfMySleeper:"+priceOfMySleeper);
+		
+		int  populationOfMaharashtra = 112374333;
+		System.out.println("populationOfMaharashtra:"+populationOfMaharashtra);
+		
+		long populationOfIndia =1350000000;
+		System.out.println("populationOfIndia:"+populationOfIndia);
+		
+		float myWeightInKilogram = 67.5f;
+		System.out.println("myWeightInKilogram:"+myWeightInKilogram);
+		
+		double areaOfMyHomeInMeters = 92.9034f;
+		System.out.println("areaOfMyHomeInMeters:"+areaOfMyHomeInMeters);
+		
+		boolean iAmStillAKid = true;
+		System.out.println("iAmStillAKid:"+iAmStillAKid);
+		
+		char startingLetterInAlphabate = 'a';
+		System.out.println("startingLetterInAlphabate:"+startingLetterInAlphabate);
+		
+		String myGamingName= "Rp";
+		System.out.println("myGamingName:"+myGamingName);
+		
+	}
+}	
+		
+		
+		
+		
---
+++ DataTypes/TypeB.java
@@ -0,0 +1,35 @@
+public class TypeB{
+	public static void main(String[] show){
+		byte daysInFebMonth = 28;
+		System.out.println("daysInFebMonth:"+daysInFebMonth);
+		
+		short myPhoneNoEndsWith = 4007;
+		System.out.println("myPhoneNoEndsWith:"+myPhoneNoEndsWith);
+		
+		int  populationOfKarnataka = 61100000;
+		System.out.println("populationOfKarnataka:"+populationOfKarnataka);
+		
+		long populationOfIndia =1350000000;
+		System.out.println("populationOfIndia:"+populationOfIndia);
+		
+		float myWeightInPounds = 148.8f;
+		System.out.println("myWeightInPounds:"+myWeightInPounds);
+		
+		double areaOfMyHomeInMeters = 92.9034f;
+		System.out.println("areaOfMyHomeInMeters:"+areaOfMyHomeInMeters);
+		
+		boolean iAmStillAKid = true;
+		System.out.println("iAmStillAKid:"+iAmStillAKid);
+		
+		char startingLetterInAlphabate = 'a';
+		System.out.println("startingLetterInAlphabate:"+startingLetterInAlphabate);
+		
+		String nameOfMyTrainee= "om";
+		System.out.println("mynameOfMyTrainee:"+nameOfMyTrainee);
+		
+	}
+}	
+		
+		
+		
+		
