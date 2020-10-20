7ba429047f45e7bfae862502d5e93d16e0c7ca21
Rohanpatil932 <72926252+Rohanpatil932@users.noreply.github.com>
2020-10-20 20:14:02 +0530

    data-Types

---
+++ Types.java
@@ -0,0 +1,35 @@
+public class Types{
+	public static void main(String[] show){
+		byte familyMembers = 4;
+		System.out.println("familyMembers:"+familyMembers);
+		
+		short populousCitiesInMaharashtra = 378;
+		System.out.println("populousCitiesInMaharashtra:"+populousCitiesInMaharashtra);
+		
+		int  longestRoadWeCanDriveInMeters = 30577536;
+		System.out.println("longestRoadWeCanDriveInMeters:"+longestRoadWeCanDriveInMeters);
+		
+		long averageCigrateSmokersInIndia =120000000;
+		System.out.println("averageCigrateSmokersInIndia:"+averageCigrateSmokersInIndia);
+		
+		float fatPercantageInMilk = 3.5f;
+		System.out.println("fatPercantageInMilk:"+fatPercantageInMilk);
+		
+		double rootOf45 = 6.7082039325f;
+		System.out.println("rootOf45:"+rootOf45);
+		
+		boolean iAmIronman = true;
+		System.out.println("iAmIronman:"+iAmIronman);
+		
+		char endingLetterInAlphabate = 'z';
+		System.out.println("endingLetterInAlphabate:"+endingLetterInAlphabate);
+		
+		String Name= "Rohan";
+		System.out.println("Name:"+Name);
+		
+	}
+}	
+		
+		
+		
+		
