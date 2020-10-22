93758b8cb1c8c4ce206b0422d506d4f3abe2f66f
Rohanpatil932 <72926252+Rohanpatil932@users.noreply.github.com>
2020-10-22 18:51:48 +0530

    array_cosole

---
+++ Avengers.java
@@ -0,0 +1,28 @@
+public class Avengers
+{
+	public static void main(String[] total)
+	{
+		System.out.println("JVM invoked main");
+		
+				System.out.println("totalMembers"+total);
+				
+				int allOfThem=total.length;
+				System.out.println("total members"+allOfThem);
+				
+			System.out.println("member at index 5"+total[5]);
+			System.out.println("member at index 6"+total[6]);
+			System.out.println("member at index 0"+total[0]);
+			System.out.println("member at index 7"+total[7]);
+			System.out.println("member at index 8"+total[8]);
+			System.out.println("member at index 9"+total[9]);
+			System.out.println("member at index 1"+total[1]);
+			System.out.println("member at index 3"+total[3]);
+			System.out.println("member at index 4"+total[4]);
+			System.out.println("member at index 2"+total[2]);
+			
+			System.out.println("JVM  main exit");
+
+	}
+	
+	
+}
